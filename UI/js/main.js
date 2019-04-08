4a581b2aca23c13e58df89ec3cf905bc5ab6a421
solomonfrank <solomonfrank73@hotmail.com>
2019-04-08 12:32:44 +0100

    changed the landing page

--- UI/js/main.js
+++ UI/js/main.js
@@ -1,13 +1,14 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 
+
 document.addEventListener('DOMContentLoaded',() =>{
-	
 	
 	
 	window.onscroll= ()=>{
 		
 		const nav_value = document.querySelector('.top-nav-wrapper').offsetHeight;
-		const second_nav = document.querySelector('.second-nav');
+        const second_nav = document.querySelector('.second-nav');
+       
 		const list_burger = document.querySelector('.icon > li');
 		
 		if( window.scrollY >= nav_value){

9f5a82a6d61b5726922b608217209ac5ff21d53a
solomonfrank <solomonfrank73@hotmail.com>
2019-04-11 03:20:26 +0100

    changed the body of the cashier

--- cashier.js
+++ cashier.js
@@ -1,54 +1,45 @@
+// const User = require("./user").default;
+import User from "./user";
+// const cashier = require("./cashier");
+// const session = require("express-session");
 
+class Cashier extends User {
+  // eslint-disable-next-line class-methods-use-this
+  debit(accNum, accData, cashier, amount) {
+    const found = accData.find(acc => acc.accNumber === accNum);
+    // console.log(found);
 
-const User = require('./user');
-const cashier = require('./cashier');
-const session = require('express-session');
+    if (!found) return false;
 
-
-class Cashier extends User{
-
-
-    debit(accNum,accData,cashier,amount){
-      let found  =  accData.find( acc => acc.accNumber === accNum);
-      console.log(found);
-      
-      if(!found) return false;
-    
-      if(found.Balance < amount){
+    if (found.Balance < amount) {
       found.Balance = found.Balance;
-    }else{
-      found.Balance = found.Balance - amount;
-    }
-      
-      return {
-        ...found,
-        cashier,
-        transactionType : 'debit',
-        transactionId : Math.floor(Math.random() * 1000000)
-
-      }
-             
-
-
+    } else {
+      found.Balance -= amount;
     }
 
-    credit(accNum,accData,cashier,amount){
-      console.log(accData);
-      let found  =  accData.find( acc => acc.accNumber === accNum);
-      console.log(found);
-      
-      if(!found) return false;
+    return {
+      ...found,
+      cashier,
+      transactionType: "debit",
+      transactionId: Math.floor(Math.random() * 1000000)
+    };
+  }
 
-      found.Balance = found.Balance + amount;
-      return {
-        ...found,
-        cashier,
-        transactionType : 'credit',
-        transactionId : Math.floor(Math.random() * 1000000)
+  // eslint-disable-next-line class-methods-use-this
+  credit(accNum, accData, cashier, amount) {
+    const found = accData.find(acc => acc.accNumber === accNum);
 
-      }
-    }
+    if (!found) return false;
+
+    found.Balance += amount;
+    return {
+      ...found,
+      cashier,
+      transactionType: "credit",
+      transactionId: Math.floor(Math.random() * 1000000)
+    };
+  }
 }
 
-module.exports = Cashier;
-
+// module.exports = Cashier;
+export default Cashier;

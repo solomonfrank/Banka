9270d902bc84f487206a88c5532cd0c2b6b4b8c6
solomonfrank <solomonfrank73@hotmail.com>
2019-04-11 11:57:51 +0100

    moved the database to the model folder

---
+++ model/database.js
@@ -0,0 +1,28 @@
+export let usersData = [
+  {
+    id: 1,
+
+    email: "solomon@yahoo.com",
+    first: "solomon",
+    last: "rock",
+    password: "123456",
+    type: "staff",
+    isAdmin: true,
+    token: "y88y8y8y8yyyyy"
+  },
+  {
+    id: 2,
+
+    email: "solomon@yahoo.com",
+    first: "solomon",
+    last: "rock",
+    password: "123456",
+    type: "staff",
+    isAdmin: true,
+    token: "y88y8y8y8yyyyy"
+  }
+];
+
+export let usersAccount = [];
+
+//export default usersData;

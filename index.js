d7e57798fb468195aea00914af01df055ca56861
solomonfrank <solomonfrank73@hotmail.com>
2019-04-11 03:15:02 +0100

    modify the index page

--- index.js
+++ index.js
@@ -1,231 +1,358 @@
-const express = require('express');
-const path = require('path');
-const uuid = require('uuid');
-const jwt = require('jsonwebtoken');
-const Admin = require('./admin');
-//const fs = require('fs');
-const usersArr = require('./database');
+/* eslint-disable consistent-return */
+import express from "express";
+import Joi from "joi";
+import session from "express-session";
+import bodyParser from "body-parser";
+import User from "./user";
+import Admin from "./admin";
+import Account from "./account";
+import Superadmin from "./superadmin";
+import Cashier from "./cashier";
 
 const app = express();
-const User = require('./user');
+// eslint-disable-next-line no-use-before-define
+app.use(bodyParser.json());
+// eslint-disable-next-line no-use-before-define
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
 
-const session = require('express-session');
-const Account = require('./account');
-const Superadmin = require('./superadmin');
-const Cashier = require('./cashier');
+// sign up route
+app.post("/api/v1/sign-up", (req, res) => {
+  const schema = {
+    firstName: Joi.string()
+      .trim()
 
+      .min(3)
+      .max(20)
+      .required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+    lastName: Joi.string()
 
-session.account =[];
-let accDb = session.account;
-// body parser middleware
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+      .trim()
+      .min(3)
+      .max(20)
+      .required(),
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .required(),
+    confirmPassword: Joi.string()
+      .required()
+      .valid(Joi.ref("password"))
+      .options({
+        language: {
+          any: {
+            allowOnly: "!!Password do not match"
+          }
+        }
+      })
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).json({ msg: result.error.details[0].message });
+    return;
+  }
+  const { email } = req.body;
+  const { password } = req.body;
+  const { firstName } = req.body;
+  const { lastName } = req.body;
 
-//set public folder
+  // let user =    new User(firstName,lastName,password,email);
+  const person = new User(firstName, lastName, password, email);
 
-app.use(express.static(path.join(__dirname,'public')));
+  const userDetail = person.save();
+  // user.save();
 
-app.post('/api/v1/sign-up',(req,res) =>{
-   
-     let email= req.body.email;
-     let password = req.body.password;
-     let firstName = req.body.firstName;
-     let lastName = req.body.lastName
-
-
-  //let user =    new User(firstName,lastName,password,email);
-  let person  =    new User(firstName,lastName,password,email);
-
- let userDetail =  person.save();
- //user.save();
-  if(!userDetail) return res.status(400).json({status:400, msg:"error in the values your submitted"})
-  res.status(200).json({status:200, data : userDetail});
-  //console.log(session.users);
-    
-
+  if (!userDetail)
+    return res
+      .status(400)
+      .json({ status: 400, msg: "error in the values your submitted" });
+  res.status(200).json({ status: 200, data: userDetail });
+  // console.log(session.users);
 });
 
 // sign in route
 
-app.post('/api/v1/sign-in',(req,res) =>{
-   
-     let email= req.body.email;
-     let password = req.body.password;
+app.post("/api/v1/sign-in", (req, res) => {
+  const schema = {
+    password: Joi.string()
+      .regex(/^[a-zA-Z0-9]{3,30}$/)
+      .trim(),
 
-  let user =   User.login(email,password);
-    if(!user) return res.status(400).json({status:400,msg:"invalid credential"});
-  
-    res.status(200).json({status:200, data : user});
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .required()
+      .trim()
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).json({ msg: result.error.details[0].message });
+    return;
+  }
+  const { email } = req.body;
+  const { password } = req.body;
+
+  const user = User.login(email, password);
+  if (!user)
+    return res.status(400).json({ status: 400, msg: "invalid credential" });
+
+  res.status(200).json({ status: 200, data: user });
 });
 
+app.post("/api/v1/create-account", (req, res) => {
+  if (session.loggedIn) {
+    const schema = {
+      firstName: Joi.string()
+        .trim()
 
-app.post('/api/v1/create-account',(req,res) =>{
-   
-  if(session.loggedIn){
-    let email= req.body.email;
-    let firstName = req.body.firstName;
-    let lastName = req.body.lastName;
-    let openingBalance = req.body.openingBalance;
-    let type = req.body.type;
+        .min(3)
+        .max(20)
+        .required(),
+      email: Joi.string().email({ minDomainAtoms: 2 }),
+      type: Joi.string().trim(),
+      openingBalance: Joi.number(),
+      lastName: Joi.string()
 
-    const person = new Account(firstName,lastName,email,type,openingBalance);
-    let user = person.save();
-    res.status(200).json(user);
+        .trim()
+        .min(3)
+        .max(20)
+        .required()
+    };
+    const result = Joi.validate(req.body, schema);
+    if (result.error) {
+      res.status(400).json({ msg: result.error.details[0].message });
+      return;
+    }
+    const { email } = req.body;
+    const { firstName } = req.body;
+    const { lastName } = req.body;
+    const { openingBalance } = req.body;
+    const { type } = req.body;
 
-  }else{
-    res.status(400).json({status:400,msg:"your must login to create an account"});
-  }  
-  //console.log(session.account);
-
-
-
+    const person = new Account(
+      firstName,
+      lastName,
+      email,
+      type,
+      openingBalance
+    );
+    const user = person.save();
+    res.status(200).json({ status: 200, data: user });
+  } else {
+    res
+      .status(400)
+      .json({ status: 400, msg: "you must login to create an account" });
+  }
+  // console.log(session.account);
 });
 
 // logout route
-app.get('/api/v1/logout',(req, res)=>{
+app.get("/api/v1/logout", (req, res) => {
+  // User.logout();
 
+  session.userId = "";
 
-  User.logout();
-  res.json({msg : 'you have successfully sign out'});
+  session.staffId = "";
+
+  session.cashierId = "";
+
+  res.json({ msg: "you have successfully sign out" });
 });
 
+// delete user route
 
-//delete user route
+app.delete("/api/v1/accounts/:accountNumber", (req, res) => {
+  if (session.staffId) {
+    // eslint-disable-next-line radix
+    // eslint-disable-next-line prettier/prettier
+    const acc = parseInt(req.params.accountNumber, 10);
 
-app.delete('/api/v1/accounts/:accountNumber',(req, res)=>{
+    const admin = new Admin();
+    const foundvalue = admin.deleteAcc(acc, session.account);
 
-  if(session.staffId){
-    let acc = parseInt(req.params.accountNumber);
-
-
-    let admin = new Admin();
-    let foundvalue =  admin.deleteAcc(acc,session.account);
-    
-   
     if (!foundvalue) {
-     return res.status(400).json({status:400, msg: "account not found"});
-     }
-    
-     res.status(200).json({status:200, msg:"account delete successfully"});
-    
-    
-  }else{
-    res.status(401).json({status:401, msg:'you must login to continue'});
+      return res.status(404).json({ status: 404, msg: "account not found" });
+    }
+
+    res.status(200).json({ status: 200, msg: "account deleted successfully" });
+  } else {
+    res.status(401).json({ status: 401, msg: "you must login to continue" });
   }
-
-
 });
 
-
-//fetch user
-app.get('/api/v1/accounts/:accountNumber',(req,res)=>{
-
-if(session.staffId || session.cashierId){
-  let acc = parseInt(req.params.accountNumber);
-  let admin = new Admin();
-  let account = admin.findOne(acc,session.account);
-  if(!account) return res.status(400).json({status:400,msg:"account not found"});
-  res.status(200).json({status:200, data:account});
-}else{
-  //User.login(email,password);
-  res.status(401).json({status:401, msg:'you must login to continue'});
-}
+// fetch user
+app.get("/api/v1/accounts/:accountNumber", (req, res) => {
+  if (session.staffId !== "" || session.cashierId !== "") {
+    const acc = parseInt(req.params.accountNumber, 10);
+    const admin = new Admin();
+    const account = admin.findOne(acc, session.account);
+    if (!account)
+      return res.status(404).json({ status: 404, msg: "account not found" });
+    res.status(200).json({ status: 200, data: account });
+  } else {
+    // User.login(email,password);
+    res.status(401).json({ status: 401, msg: "you must login to continue" });
+  }
 });
 // patch user
-app.patch('/api/v1/accounts/:accountNumber',(req,res)=>{
-  let acc = parseInt(req.params.accountNumber);
- if(session.staffId){
-  let admin = new Admin();
-let arr = admin.activateAcc(acc,session.account);
-if(!arr) return res.status(400).json({status:400,msg: 'account not found'});
+app.patch("/api/v1/accounts/:accountNumber", (req, res) => {
+  const acc = parseInt(req.params.accountNumber, 10);
+  if (session.staffId) {
+    const admin = new Admin();
+    const arr = admin.activateAcc(acc, session.account);
+    if (!arr)
+      return res.status(404).json({ status: 404, msg: "account not found" });
 
-res.status(200).json({status:200, data:arr});
- 
-}else{
-   //User.login(email,password);
-  return  res.status(401).json({status:401, msg:'you must login to continue'});
- }
-  
-
-
+    res.status(200).json({ status: 200, data: arr });
+  } else {
+    // User.login(email,password);
+    return res
+      .status(401)
+      .json({ status: 401, msg: "you must login to continue" });
+  }
 });
 
+app.post("/api/v1/add-admin", (req, res) => {
+  const schema = {
+    firstName: Joi.string()
+      .trim()
 
-app.post('/api/v1/add-admin', (req,res)=>{
-let firstName = req.body.firstName;
-let email = req.body.email;
-let lastName = req.body.lastName;
-let type = req.body.type;
-let password = req.body.password;
-let isAdmin = req.body.isAdmin;
+      .min(3)
+      .max(20)
+      .required(),
+    password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
+    lastName: Joi.string()
 
-//let admin = new Superadmin();
+      .trim()
+      .min(3)
+      .max(20)
+      .required(),
+    email: Joi.string()
+      .email({ minDomainAtoms: 2 })
+      .required(),
+    type: Joi.string()
 
- let lastInserted =   Superadmin.addStaff(firstName,lastName,password,email,type = type,isAdmin = isAdmin );
- if(!lastInserted){
-     res.status(400).json({msg:"user could not added"});
- }
-    
- res.status(200).json({status:200, data: lastInserted});
- //console.log(session.users);
+      .trim()
+      .min(3)
+      .max(20)
+      .required(),
+    isAdmin: Joi.boolean().required(),
+    confirmPassword: Joi.string()
+      .required()
+      .valid(Joi.ref("password"))
+      .options({
+        language: {
+          any: {
+            allowOnly: "!!Passwords do not match"
+          }
+        }
+      })
+  };
+  const result = Joi.validate(req.body, schema);
+  if (result.error) {
+    res.status(400).json({ msg: result.error.details[0].message });
+    return;
+  }
+  const { firstName } = req.body;
+  const { email } = req.body;
+  const { lastName } = req.body;
+  let { type } = req.body;
+  const { password } = req.body;
+  let { isAdmin } = req.body;
+
+  // let admin = new Superadmin();
+
+  const lastInserted = Superadmin.addStaff(
+    firstName,
+    lastName,
+    password,
+    email,
+    // eslint-disable-next-line no-self-assign
+    (type = type),
+    // eslint-disable-next-line no-self-assign
+    (isAdmin = isAdmin)
+  );
+  if (!lastInserted) {
+    res.status(400).json({ msg: "user could not added" });
+  }
+
+  res.status(200).json({ status: 200, data: lastInserted });
+  // console.log(session.users);
 });
 
+// credit account
+app.post("/api/v1/transaction/:accountNumber/credit", (req, res) => {
+  if (session.cashierId || session.staffId) {
+    const accNum = parseInt(req.params.accountNumber, 10);
+    const { amount } = req.body;
 
-
-
-//credit account
-app.post('/api/v1/transaction/:accountNumber/credit',(req,res)=>{
-if(session.cashierId){
-    let accNum = parseInt(req.params.accountNumber);
-    let amount = req.body.amount;
-
-    let cashier = new Cashier();
-  let credited =  cashier.credit(accNum,session.account,session.cashierId,amount);
-  console.log(credited);
-  if(!credited) return res.status(404).json({status:404, msg : "account not found"});
-   res.status(200).json({status:200, data : credited});
-}else{
-  
-  return res.status(403).json({status:403,msg:"you must login to accessible the page"})
-}
+    const cashier = new Cashier();
+    const credited = cashier.credit(
+      accNum,
+      session.account,
+      session.cashierId,
+      amount
+    );
+    // eslint-disable-next-line no-console
+    console.log(credited);
+    if (!credited)
+      return res.status(404).json({ status: 404, msg: "account not found" });
+    res.status(200).json({ status: 200, data: credited });
+  } else {
+    return res
+      .status(403)
+      .json({ status: 403, msg: "you must login to accessible the page" });
+  }
 });
-
 
 // debit account
-app.post('/api/v1/transaction/:accountNumber/debit',(req,res)=>{
-  if(session.cashierId){
-      let accNum = parseInt(req.params.accountNumber);
-      let amount = req.body.amount;
-  
-      let cashier = new Cashier();
-    let credited =  cashier.debit(accNum,session.account,session.cashierId,amount);
+app.post("/api/v1/transaction/:accountNumber/debit", (req, res) => {
+  if (session.cashierId || session.staffId) {
+    const accNum = parseInt(req.params.accountNumber, 10);
+    const { amount } = req.body;
+
+    const cashier = new Cashier();
+    const credited = cashier.debit(
+      accNum,
+      session.account,
+      session.cashierId,
+      amount
+    );
+    // eslint-disable-next-line no-console
     console.log(credited);
-    if(!credited) return res.status(404).json({status:404, msg : "account not found"});
-     res.status(200).json({status:200, data : credited});
-  }else{
-    
-    return res.status(403).json({status:403,msg:"you must login to accessible the page"})
+    if (!credited)
+      return res.status(404).json({ status: 404, msg: "account not found" });
+    res.status(200).json({ status: 200, data: credited });
+  } else {
+    return res
+      .status(403)
+      .json({ status: 403, msg: "you must login to accessible the page" });
   }
-  });
-
-
-  //fetch all account
-
-  app.get('/api/v1/accounts', (req, res)=>{
-   if(session.staffId){
-    const admin = new Admin();
-    let result = admin.findAll();
-    if(!result) return res.status(404).json({status:404,msg : "result not found"});
-
-    res.status(200).json({status:200,data : result});
-   }else{
-     return res.status(400).json({status:400,msg : 'you must login'})
-   }
- 
-  });
-
-//Set environment Port
-let PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log('listening to port ' + PORT);
 });
+
+// fetch all account
+
+app.get("/api/v1/accounts", (req, res) => {
+  if (session.staffId) {
+    const admin = new Admin();
+    const result = admin.findAll();
+    if (!result)
+      return res.status(404).json({ status: 404, msg: "result not found" });
+
+    res.status(200).json({ status: 200, data: result });
+  } else {
+    return res.status(400).json({ status: 400, msg: "you must login" });
+  }
+});
+
+// Set environment Port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`listening to port ${PORT}`);
+});
+
+// module.exports = app;
+export default app;

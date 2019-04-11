c96477ac13ab64ba686370f359fdad9cb9f6e3b4
solomonfrank <solomonfrank73@hotmail.com>
2019-04-11 11:01:37 +0100

    created array of object

--- database.js
+++ database.js
@@ -1,5 +1,28 @@
+export let usersData = [
+  {
+    id: 1,
 
+    email: "solomon@yahoo.com",
+    first: "solomon",
+    last: "rock",
+    password: "123456",
+    type: "staff",
+    isAdmin: true,
+    token: "y88y8y8y8yyyyy"
+  },
+  {
+    id: 2,
 
-let usersData = [];
+    email: "solomon@yahoo.com",
+    first: "solomon",
+    last: "rock",
+    password: "123456",
+    type: "staff",
+    isAdmin: true,
+    token: "y88y8y8y8yyyyy"
+  }
+];
 
-module.exports = usersData;
+export let usersAccount = [];
+
+//export default usersData;

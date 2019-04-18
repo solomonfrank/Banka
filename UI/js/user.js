2d6aa8f93d77bea84fe194a946630c05e0bd1702
solomonfrank <solomonfrank73@hotmail.com>
2019-04-18 05:38:46 +0100

    feature(UI modification):modified the UI

--- UI/js/user.js
+++ UI/js/user.js
@@ -1,14 +1,23 @@
-/* eslint-disable no-undef */
 
-// eslint-disable-next-line no-undef
-document.querySelector('.burger-list').onclick = () => {
+
+document.querySelector('.dropdown-btn').onclick = () => {
   // eslint-disable-next-line no-undef
+
+  document.querySelector('.dropdown').classList.toggle('drop');
+};
+document.querySelector('.credit').onclick = () => {
+  // eslint-disable-next-line no-undef
+  //alert('hello')
+  //document.querySelector('.dropdown').classList.toggle('drop');
+};
+document.querySelector('.burger-list').onclick = () => {
+
   document.querySelector('.sidebar-small').classList.toggle('side');
 };
 
 
 const modal = document.querySelector('#myModal');
-const btn = document.querySelector('#btn') || document.querySelector('.add-role') || document.querySelector('#add-btn');
+const btn = document.querySelector('#btn') || document.querySelector('.add-role') || document.querySelector('#add-btn') || document.querySelector('.credit')
 
 const span = document.querySelector('.close');
 btn.onclick = () => {
@@ -24,3 +33,28 @@ window.onclick = (event) => {
     modal.style.display = 'none';
   }
 };
+
+
+//debit 
+
+
+const modalDebit = document.querySelector('.modal-debit');
+const btnDebit = document.querySelector('.debit');
+
+const spanx = document.querySelector('#close-debit');
+btnDebit.onclick = () => {
+
+  modalDebit.style.display = 'block';
+};
+spanx.onclick = () => {
+  modalDebit.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+
+window.onclick = (event) => {
+  if (event.target === modalDebit) {
+    modalDebit.style.display = 'none';
+  }
+};
+

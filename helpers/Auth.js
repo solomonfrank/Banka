d11f82484a02378281670da2b5770ccbc8e52a66
solomonfrank <solomonfrank73@hotmail.com>
2019-05-06 00:25:42 +0100

    added jwt token verification

--- helpers/Auth.js
+++ helpers/Auth.js
@@ -16,19 +16,28 @@ const Auth = {
 
   // eslint-disable-next-line consistent-return
   async verifyToken(req, res, next) {
-    const token = req.header['x-access-token'];
-    console.log(token);
+    let token = req.headers['x-access-token'];
+
+    token = token.trim();
+
     if (!token) {
       return Response.onError(res, 400, 'Token not found');
     }
     try {
       const decoded = await jwt.verify(token, process.env.SECRET_KEY);
-      const { userId } = decoded.key.data;
-      const result = await User.init().findById(userId, '*');
+
+      const { id } = decoded.key;
+
+      const params = { id };
+
+      const result = await User.init().find(params);
+
       if (!result.rows[0]) {
         return Response.onError(res, 400, 'invalid token provided');
       }
-      req.userData = decoded.key;
+
+
+      req.userData = params;
       next();
     } catch (err) {
       return Response.onError(res, 500, 'Internal server error');
